7e35c5dc156ee39394fefe91958c12cc51318fc0
s2ul98 <totolo402@naver.com>
2025-02-25 16:47:38 +0900

    추가

--- src/main/Ex1.java
+++ src/main/Ex1.java
@@ -4,10 +4,52 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 		
+		// 인터페이스는 불완전한 구조체이므로 인스턴스 생성불가
+//		Calc calc = new calc();
+		
+		// 자식 클래스로 인스턴스를 생성할 것
+		Calculator calculator = new Calculator();
+	
+		int result1 = calculator.add(10, 5);
+		System.out.println(result1);
+		int result2 = calculator.substract(10, 5);
+		System.out.println(result2);
+		int result3 = calculator.times(10, 5);
+		System.out.println(result3);
+		int result4 = calculator.divide(10, 5);
+		System.out.println(result4);
+		
 
 	}
 
 }
-class Calculator implements {
+// 계산기 클래스
+// C -> C (extends)
+// C -> I (implements)
+class Calculator implements Calc {
+
+	@Override
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	@Override
+	public int times(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	@Override
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	public void showInfo() {
+		System.out.println("계산기 클래스 입니다");
+	}
 	
 }
---
+++ src/main/Ex2.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		// 형변환
+		// 부모타입 변수 = 자식 인스턴스
+		Calc calc = new Calculator();
+		
+		// 형변환된 변수는 부모가 물려준 함수만 사용할 수 있다
+//		calc.showInfo();
+
+	}
+
+}
+
+
+
